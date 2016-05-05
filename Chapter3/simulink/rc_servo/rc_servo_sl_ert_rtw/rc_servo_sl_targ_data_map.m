8bc50b332fde3d6892aabcdbfc370efeb3ff7bb5
Masaru Morita <masamorobo@gmail.com>
2016-05-05 21:25:09 +0900

    [add] サーボモータ制御Simulink版追加

---
+++ Chapter3/simulink/rc_servo/rc_servo_sl_ert_rtw/rc_servo_sl_targ_data_map.m
@@ -0,0 +1,221 @@
+  function targMap = targDataMap(),
+
+  ;%***********************
+  ;% Create Parameter Map *
+  ;%***********************
+      
+    nTotData      = 0; %add to this count as we go
+    nTotSects     = 3;
+    sectIdxOffset = 0;
+    
+    ;%
+    ;% Define dummy sections & preallocate arrays
+    ;%
+    dumSection.nData = -1;  
+    dumSection.data  = [];
+    
+    dumData.logicalSrcIdx = -1;
+    dumData.dtTransOffset = -1;
+    
+    ;%
+    ;% Init/prealloc paramMap
+    ;%
+    paramMap.nSections           = nTotSects;
+    paramMap.sectIdxOffset       = sectIdxOffset;
+      paramMap.sections(nTotSects) = dumSection; %prealloc
+    paramMap.nTotData            = -1;
+    
+    ;%
+    ;% Auto data (ajpfph4kivz)
+    ;%
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% ajpfph4kivz.ServoWrite_pinNumber
+	  section.data(1).logicalSrcIdx = 0;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      paramMap.sections(1) = section;
+      clear section
+      
+      section.nData     = 4;
+      section.data(4)  = dumData; %prealloc
+      
+	  ;% ajpfph4kivz.SineWave_Amp
+	  section.data(1).logicalSrcIdx = 1;
+	  section.data(1).dtTransOffset = 0;
+	
+	  ;% ajpfph4kivz.SineWave_Bias
+	  section.data(2).logicalSrcIdx = 2;
+	  section.data(2).dtTransOffset = 1;
+	
+	  ;% ajpfph4kivz.SineWave_NumSamp
+	  section.data(3).logicalSrcIdx = 3;
+	  section.data(3).dtTransOffset = 2;
+	
+	  ;% ajpfph4kivz.SineWave_Offset
+	  section.data(4).logicalSrcIdx = 4;
+	  section.data(4).dtTransOffset = 3;
+	
+      nTotData = nTotData + section.nData;
+      paramMap.sections(2) = section;
+      clear section
+      
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% ajpfph4kivz.ServoWrite_p1
+	  section.data(1).logicalSrcIdx = 5;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      paramMap.sections(3) = section;
+      clear section
+      
+    
+      ;%
+      ;% Non-auto Data (parameter)
+      ;%
+    
+
+    ;%
+    ;% Add final counts to struct.
+    ;%
+    paramMap.nTotData = nTotData;
+    
+
+
+  ;%**************************
+  ;% Create Block Output Map *
+  ;%**************************
+      
+    nTotData      = 0; %add to this count as we go
+    nTotSects     = 1;
+    sectIdxOffset = 0;
+    
+    ;%
+    ;% Define dummy sections & preallocate arrays
+    ;%
+    dumSection.nData = -1;  
+    dumSection.data  = [];
+    
+    dumData.logicalSrcIdx = -1;
+    dumData.dtTransOffset = -1;
+    
+    ;%
+    ;% Init/prealloc sigMap
+    ;%
+    sigMap.nSections           = nTotSects;
+    sigMap.sectIdxOffset       = sectIdxOffset;
+      sigMap.sections(nTotSects) = dumSection; %prealloc
+    sigMap.nTotData            = -1;
+    
+    ;%
+    ;% Auto data (ps4si3ezfui)
+    ;%
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% ps4si3ezfui.nhfng0xxzf
+	  section.data(1).logicalSrcIdx = 0;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      sigMap.sections(1) = section;
+      clear section
+      
+    
+      ;%
+      ;% Non-auto Data (signal)
+      ;%
+    
+
+    ;%
+    ;% Add final counts to struct.
+    ;%
+    sigMap.nTotData = nTotData;
+    
+
+
+  ;%*******************
+  ;% Create DWork Map *
+  ;%*******************
+      
+    nTotData      = 0; %add to this count as we go
+    nTotSects     = 2;
+    sectIdxOffset = 1;
+    
+    ;%
+    ;% Define dummy sections & preallocate arrays
+    ;%
+    dumSection.nData = -1;  
+    dumSection.data  = [];
+    
+    dumData.logicalSrcIdx = -1;
+    dumData.dtTransOffset = -1;
+    
+    ;%
+    ;% Init/prealloc dworkMap
+    ;%
+    dworkMap.nSections           = nTotSects;
+    dworkMap.sectIdxOffset       = sectIdxOffset;
+      dworkMap.sections(nTotSects) = dumSection; %prealloc
+    dworkMap.nTotData            = -1;
+    
+    ;%
+    ;% Auto data (npqgscs1qwa)
+    ;%
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% npqgscs1qwa.dcqpaw2c3n.LoggedData
+	  section.data(1).logicalSrcIdx = 0;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      dworkMap.sections(1) = section;
+      clear section
+      
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% npqgscs1qwa.nbnxyigmtv
+	  section.data(1).logicalSrcIdx = 1;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      dworkMap.sections(2) = section;
+      clear section
+      
+    
+      ;%
+      ;% Non-auto Data (dwork)
+      ;%
+    
+
+    ;%
+    ;% Add final counts to struct.
+    ;%
+    dworkMap.nTotData = nTotData;
+    
+
+
+  ;%
+  ;% Add individual maps to base struct.
+  ;%
+
+  targMap.paramMap  = paramMap;    
+  targMap.signalMap = sigMap;
+  targMap.dworkMap  = dworkMap;
+  
+  ;%
+  ;% Add checksums to base struct.
+  ;%
+
+
+  targMap.checksum0 = 968868534;
+  targMap.checksum1 = 3012619120;
+  targMap.checksum2 = 3008029555;
+  targMap.checksum3 = 3194245262;
+
