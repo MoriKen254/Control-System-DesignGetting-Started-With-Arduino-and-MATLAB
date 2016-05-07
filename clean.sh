1a90693a5428affe8b6e4ecc8907a3ad94650c3d
Masaru Morita <masamorobo@gmail.com>
2016-05-07 19:12:23 +0900

    [add] PI制御を追加

--- clean.sh
+++ clean.sh
@@ -2,11 +2,13 @@
 find . -name "*.eep" -exec git rm {} \;
 find . -name "*.elf" -exec git rm {} \;
 find . -name "*.hex" -exec git rm {} \;
+find . -name "*.autosave" -exec git rm {} \;
 find . -name "slprj" -exec git rm -r {} \;
 find . -name "*_ert_rtw" -exec git rm -r {} \;
 
 find . -name "*.eep" -exec rm {} \;
 find . -name "*.elf" -exec rm {} \;
 find . -name "*.hex" -exec rm {} \;
+find . -name "*.autosave" -exec rm {} \;
 find . -name "slprj" -exec rm -r {} \;
 find . -name "*_ert_rtw" -exec rm -r {} \;
